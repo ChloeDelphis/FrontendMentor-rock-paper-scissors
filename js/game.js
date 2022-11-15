9243bf9ad0aa4816b8d5cfaa9567917aff9a4000
Chloe Delphis <chloe.delphis@gmail.com>
2022-11-16 00:48:47 +0100

    javascript done (scores)

--- js/game.js
+++ js/game.js
@@ -1,60 +1,66 @@
+//! vérifier les let et const partout (dans les fonctions)
+
 const game = {
   init: function () {
-    const paperElmt = document.querySelector("#blue");
-    const scissorsElmt = document.querySelector("#yellow");
-    const rockElmt = document.querySelector("#red");
+    // sessionStorage.setItem("score", this.score);
+    // condition ? exprSiVrai : exprSiFaux
 
-    paperElmt.addEventListener("click", game.playGame);
-    scissorsElmt.addEventListener("click", game.playGame);
-    rockElmt.addEventListener("click", game.playGame);
+    game.showScore(),
+      (choicesElmts = document.querySelectorAll(".container__game__circle"));
 
-    // sessionStorage.setItem("score", "0");
+    for (choice of choicesElmts) {
+      choice.addEventListener("click", game.playGame);
+    }
+
+    playAgainElmt = document.querySelector(".results__result__play-again");
+    playAgainElmt.addEventListener("click", game.displayGame);
   },
 
   playGame(event) {
-    // Gets the choice picked by user
-    let = userChoice = event.currentTarget.getAttribute("id");
-    console.log("user picked : " + userChoice);
+    gameElmt = document.querySelector(".container");
+    console.log(gameElmt);
+    gameElmt.classList.remove("hidden");
 
-    // Computer picks a choice
-    let computerPick = game.computerPicks();
-    console.log("computer picked : " + computerPick);
+    resultsElmt = document.querySelector(".results");
+    resultsElmt.classList.add("hidden");
+
+    // Gets the choice picked by the user: color + shape
+    let userColor = event.currentTarget.getAttribute("id");
+    let userShape = event.currentTarget.dataset.shape;
+    console.log("user color : " + userColor);
+    console.log("user shape : " + userShape);
+
+    // Gets the choice picked by the computer: color + shape
+    let computerColor = game.computerPicks();
+    let computerShape = document.querySelector("#" + computerColor).dataset
+      .shape;
+    console.log("computer color : " + computerColor);
+    console.log("computer shape : " + computerShape);
 
     // Display choices
-    game.displayChoices(userChoice, computerPick);
+    game.displayChoices(userColor, userShape, computerColor, computerShape);
 
     // Compares choices and designates winner calls function to update score
-    if (userChoice === computerPick) {
+    if (userShape === computerShape) {
+      game.displayResult("Draw");
       console.log("Draw");
-      //   game.draw();
     } else if (
-      (userChoice === "blue" && computerPick === "yellow") ||
-      (userChoice === "red" && computerPick === blue) ||
-      (userChoice === "yellow" && computerPick === "red")
+      (userShape === "paper" && computerShape === "scissors") ||
+      (userShape === "rock" && computerShape === "paper") ||
+      (userShape === "scissors" && computerShape === "rock")
     ) {
-      console.log("You lose");
-      //   game.userLoses();
+      game.displayResult("You lose");
+      game.updateScore(-1);
 
-      return "You lose";
+      console.log("You lose");
     } else {
+      game.displayResult("You win");
+
+      game.updateScore(1);
+
       console.log("You win");
-      //   game.updateScore(1);
-      //   game.userWins();
-      return "You win";
     }
   },
-
-  //   draw() {
-  //     game.updateScore(0);
-  //   },
-
-  //   userWins() {
-  //     game.updateScore(1);
-  //   },
-
-  //   userLoses() {
-  //     game.updateScore(-1);
-  //   },
 
   // Makes computer pick a color
   computerPicks() {
@@ -64,13 +70,37 @@ const game = {
     return choices[computerPick];
   },
 
-  // Updates score
-  //   updateScore(a) {
-  //     let score = score + a;
-  //   },
+  //! Initializes score
+  showScore() {
+    if (sessionStorage.getItem("score") == null) {
+      sessionStorage.setItem("score", 0);
+    }
+    let score = parseInt(sessionStorage.getItem("score"));
+    this.displayScore(score);
+  },
 
-  displayChoices(userChoice, computerPick) {
-    console.log("display choices est appelé");
+  //! Updates score
+  updateScore(a) {
+    score = parseInt(sessionStorage.getItem("score"));
+    console.log("score before = " + score);
+    console.log("typeof = " + typeof score);
+
+    score += a;
+    console.log("score after = " + score);
+
+    console.log("score = " + score);
+    sessionStorage.setItem("score", score);
+    this.displayScore(score);
+  },
+
+  //! Displays score
+  displayScore(score) {
+    scoreElmt = document.querySelector(".header__score__points");
+    scoreElmt.innerText = score;
+  },
+
+  displayChoices(userColor, userShape, computerColor, computerShape) {
+    // console.log("display choices est appelé");
 
     // hides game area
     gameElmt = document.querySelector(".container");
@@ -80,6 +110,39 @@ const game = {
     resultsElmt = document.querySelector(".results");
     resultsElmt.classList.toggle("hidden");
 
-    // displays user's choice
+    // user's choice: image + shape
+    userCircleElmt = document.querySelector(
+      ".results__choice__singular.user div"
+    );
+    userCircleElmt.classList.remove("red", "yellow", "blue");
+    userCircleElmt.classList.add(userColor);
+
+    userImgElmt = userCircleElmt.querySelector("img");
+    userImgElmt.src = "images/icon-" + userShape + ".svg";
+
+    // displays computer's choice: image + shape
+    computerCircleElmt = document.querySelector(
+      ".results__choice__singular.computer div"
+    );
+    console.log(computerCircleElmt);
+    computerCircleElmt.classList.remove("red", "yellow", "blue");
+    computerCircleElmt.classList.add(computerColor);
+    computerImgElmt = computerCircleElmt.querySelector("img");
+    computerImgElmt.src = "images/icon-" + computerShape + ".svg";
+  },
+
+  displayResult(result) {
+    const resultElmt = document.querySelector(".results__result__text");
+    resultElmt.innerText = result;
+  },
+
+  displayGame() {
+    // displays game area
+    gameElmt = document.querySelector(".container");
+    gameElmt.classList.toggle("hidden");
+
+    // hides results area
+    resultsElmt = document.querySelector(".results");
+    resultsElmt.classList.toggle("hidden");
   },
 };
