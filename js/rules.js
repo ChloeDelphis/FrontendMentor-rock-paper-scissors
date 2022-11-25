89bc3b935eb61ba93097d10cb8931151f8e3d64a
Chloe Delphis <chloe.delphis@gmail.com>
2022-11-25 13:31:07 +0100

    winner-halo wip

--- js/rules.js
+++ js/rules.js
@@ -1,13 +1,28 @@
 const rules = {
   init: function () {
-    const rulesBtn = document.querySelector(".seerules");
-    const rulesCross = document.querySelector(".rules__cross");
-    rulesBtn.addEventListener("click", rules.displayRules);
-    rulesCross.addEventListener("click", rules.displayRules);
+    // Selects both button seerules and button cross
+    const rulesBtnAndCross = document.querySelectorAll(
+      ".seerules, .rules__cross"
+    );
+    for (btn of rulesBtnAndCross) {
+      btn.addEventListener("click", () => {
+        rules.displayRules();
+        rules.darkenOthers();
+      });
+    }
   },
 
+  /* Displays rules + Calls darkenOthers */
   displayRules: function () {
     const rulesElmnt = document.querySelector(".rules");
     rulesElmnt.classList.toggle("hidden");
+  },
+
+  /* Darkens others elements when rules is open */
+  darkenOthers: function () {
+    const everything = document.querySelectorAll("body > * :not(.rules *)");
+    everything.forEach((element) => {
+      element.classList.toggle("shaded");
+    });
   },
 };
